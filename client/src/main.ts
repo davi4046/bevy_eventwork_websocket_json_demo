d24d40381306522dde21cbeaa6c1ad56d42382f0
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-06-30 16:49:10 +0200

    Create client connection logic

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,32 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sendMessageButton = document.getElementById('send-message-button')!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const socket = new WebSocket('ws://localhost:8081');
+
+socket.addEventListener('message', async (event) => {
+    const blob = event.data as Blob;
+    console.log('Received message:', await blob.text());
+    // Server will say hello when we join
+});
+
+sendMessageButton.addEventListener('click', (_) => {
+    const data = (() => {
+        const obj = {
+            message: 'hello',
+        };
+        const string = JSON.stringify(obj);
+        const encoder = new TextEncoder();
+        const byteArray = encoder.encode(string);
+        return Array.from(byteArray);
+    })();
+
+    const message = {
+        kind: 'balls_of_steel:ChatMessage',
+        data: data,
+    };
+
+    console.log('Sending message:', JSON.stringify(message));
+
+    socket.send(JSON.stringify(message));
+});
