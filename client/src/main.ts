b9e8918053a3aef8fed68ca706dd22832657a62d
Noah <noahshomette@gmail.com>
2024-06-30 21:18:27 -0400

    testing

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,7 +10,7 @@ socket.addEventListener('message', async (event) => {
     // Server will say hello when we join
 });
 
-sendMessageButton.addEventListener('click', (_) => {
+sendMessageButton.addEventListener('click', async (_) => {
     const data = (() => {
         const obj = {
             message: 'hello',
@@ -26,7 +26,49 @@ sendMessageButton.addEventListener('click', (_) => {
         data: data,
     };
 
-    console.log('Sending message:', JSON.stringify(message));
+    let blob = new Blob([JSON.stringify(message)], {
 
-    socket.send(JSON.stringify(message));
+    });
+
+    const encoder = new TextEncoder();
+    const byteArray = encoder.encode(JSON.stringify(message));
+    let array = Array.from(byteArray)
+    let bytesArray = new Uint8Array(array);
+    console.log('Sending message:', blob);
+
+    socket.send(bytesArray);
 });
+
+function toUTF8Array(str: String) {
+    var utf8 = [];
+    for (var i = 0; i < str.length; i++) {
+        var charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+        } else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(
+                0xe0 | (charcode >> 12),
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f)
+            );
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode =
+                0x10000 +
+                (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+            utf8.push(
+                0xf0 | (charcode >> 18),
+                0x80 | ((charcode >> 12) & 0x3f),
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f)
+            );
+        }
+    }
+    return utf8;
+}
